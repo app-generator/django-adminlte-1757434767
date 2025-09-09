4a6c4cc68ebdf0006a5aa5e58e1ed46c295d7a2b
App Generator <51070104+app-generator@users.noreply.github.com>
2025-09-09 19:19:36 +0300

    initial commit

---
+++ home/models.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    iris_code = models.TextField(max_length=255, null=True, blank=True)
+    student_id = models.IntegerField(null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Attendance(models.Model):
+
+    #__Attendance_FIELDS__
+    student_id = models.IntegerField(null=True, blank=True)
+    timestamp = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    status = models.TextField(max_length=255, null=True, blank=True)
+
+    #__Attendance_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Attendance")
+        verbose_name_plural = _("Attendance")
+
+
+
+#__MODELS__END
